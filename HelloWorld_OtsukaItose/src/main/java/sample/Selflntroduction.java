922932595691ab709f0414180dd655982ca26b8e
81808 <81808@LAPTOP-B0VQCFIK.local>
2026-01-16 20:43:26 +0900

    2-3java導入※必読

---
+++ HelloWorld_OtsukaItose/src/main/java/sample/Selflntroduction.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class Selflntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	String name ="大塚愛世";
+	int age = 24;
+	String hobby = "食べること";
+	
+	/*
+	 * コメント内容
+	 */
+	
+	System.out.println("私の名前は"+name+"です。");
+	System.out.println("年齢は"+age+"歳です。");
+	System.out.println("趣味は"+hobby+"です。");}
+
+}
